2def3d1d58b0d7239a18ea6649587968402744e8
pudders <ericpudney@gmail.com>
2023-10-31 16:48:04 +0100

    styling, fixed commas and spacing for tags, added form (not yet functional)

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 fetch('https://dummyjson.com/posts')
-.then(res => res.json())
-.then(function (response) {
-  createPosts(response.posts);
-});
+  .then(res => res.json())
+  .then(function (response) {
+    createPosts(response.posts);
+  });
 
 
 const mainPage = document.getElementById("posts");
 
 function createPosts(data) {
-    for (let obj of data) {
-        let newPost = document.createElement("div");
-        newPost.classList.add("post");
-        let title = document.createElement("h2");
-        title.innerText = obj.title;
-        let body = document.createElement("p");
-        body.innerText = obj.body;
-        let tags = document.createElement("span");
-        tags.innerText = obj.tags;
-        newPost.append(title, body, tags);
-        mainPage.append(newPost);
+  for (let obj of data) {
+    let newPost = document.createElement("div");
+    newPost.classList.add("post");
+    let title = document.createElement("h2");
+    title.innerText = obj.title;
+    let body = document.createElement("p");
+    body.innerText = obj.body;
+    let tags = document.createElement("span");
+    let text = ""
+    for (i = 0; i < obj.tags.length; i++) {
+      text = text + obj.tags[i] + ", ";
     }
+    text = text.slice(0, text.length - 2);
+    tags.innerText = text;
+    newPost.append(title, body, tags);
+    mainPage.append(newPost);
+  }
 }
+
