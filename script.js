60d196aa89cf3cc6157166f7d37b29b29b0ea331
pudders <ericpudney@gmail.com>
2023-11-15 10:53:36 +0100

    added like function

--- script.js
+++ script.js
@@ -18,13 +18,24 @@ function createPosts(data) {
     let body = document.createElement("p");
     body.innerText = obj.body;
     let tags = document.createElement("span");
-    let text = ""
+    //puts commas and spaces between tags
+    let text = "";
     for (i = 0; i < obj.tags.length; i++) {
       text = text + obj.tags[i] + ", ";
     }
     text = text.slice(0, text.length - 2);
     tags.innerText = text;
-    newPost.append(title, body, tags);
+    //creates likes button and counter
+    let react = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = "images/thumb.jpg";
+    let likes = document.createElement("button");
+    likes.innerText = obj.reactions;
+    react.append(img, likes);
+    react.addEventListener("click", () => {
+      likes.innerText = parseInt(likes.innerText) + 1;
+    });
+    newPost.append(title, body, tags, react);
     mainPage.append(newPost);
   }
 }
@@ -49,7 +60,16 @@ function addNew() {
   body.innerText = newBody.value;
   let tags = document.createElement("span");
   tags.innerText = newTags.value;
-  newPost.append(title, body, tags);
+  let react = document.createElement("div");
+  let img = document.createElement("img");
+  img.src = "images/thumb.jpg";
+  let likes = document.createElement("button");
+  likes.innerText = 0;
+  react.append(img, likes);
+  react.addEventListener("click", () => {
+    likes.innerText = parseInt(likes.innerText) + 1;
+  });
+  newPost.append(title, body, tags, react);
   mainPage.append(newPost);
   newTitle.value = "";
   newBody.value = "";
