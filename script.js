ffac701df0ae1d7e6988503ec90dc8c2b9c481c9
pudders <ericpudney@gmail.com>
2023-11-15 13:21:51 +0100

    created array to save to local storage

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
+// defines area for posts to appear in and array for storing posts
+const mainPage = document.getElementById("posts");
+const postStorage =  [];
+
 // gets initial data from dummyJSON and renders it in HTML doc
 fetch('https://dummyjson.com/posts')
   .then(res => res.json())
   .then(function (response) {
-    createPosts(response.posts);
+    fillArray(response.posts);
   });
 
-// defines area for posts to appear in
-const mainPage = document.getElementById("posts");
+function fillArray(data) {
+  for (let obj of data) {
+    const newObj = {};
+    newObj.title = obj.title;
+    newObj.body = obj.body;
+    newObj.tags = obj.tags;
+    newObj.reactions = obj.reactions;
+    postStorage.push(newObj);
+  }
+  renderPosts(postStorage);
+}
 
-// turns posts into HTML objects
-function createPosts(data) {
+function renderPosts(arr) {
+  for (let i=0; i<arr.length; i++) {
+    let newPost = document.createElement("div");
+    newPost.classList.add("post");
+    let title = document.createElement("h2");
+    title.innerText = arr[i].title;
+    let body = document.createElement("p");
+    body.innerText = arr[i].body;
+    let tags = document.createElement("span");
+    //puts commas and spaces between tags
+    let text = "";
+    for (j = 0; j < arr[i].tags.length; j++) {
+      text = text + arr[i].tags[j] + ", ";
+    }
+    text = text.slice(0, text.length - 2);
+    tags.innerText = text;
+    //creates likes button and counter
+    let react = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = "images/thumb.jpg";
+    let likes = document.createElement("button");
+    likes.innerText = arr[i].reactions;
+    react.append(img, likes);
+    react.addEventListener("click", () => {
+      likes.innerText = parseInt(likes.innerText) + 1;
+    });
+    newPost.append(title, body, tags, react);
+    mainPage.append(newPost);
+  }
+}
+
+// turns posts into HTML objects - previously used to create posts directly from JSON object
+/*function createPosts(data) {
   for (let obj of data) {
     let newPost = document.createElement("div");
     newPost.classList.add("post");
@@ -38,7 +82,7 @@ function createPosts(data) {
     newPost.append(title, body, tags, react);
     mainPage.append(newPost);
   }
-}
+}*/
 
 // defines fields in form to add new posts
 const newTitle = document.getElementById("newTitle");
@@ -47,31 +91,25 @@ const newTags = document.getElementById("newTags");
 const addPost = document.getElementById("addPost");
 addPost.addEventListener("click", addNew);
 
-// cannot get createPosts to work on the form data for some reason so the code is repetitive here :(
+// adds new posts and saves array of posts to local storage
+let newMsg = { title: "", body: "", tags: "", reactions: 0 };
 function addNew() {
   if (newTitle.value === "" || newBody.value === "" || newTags.value === "") {
     return;
   }
-  let newPost = document.createElement("div");
-  newPost.classList.add("post");
-  let title = document.createElement("h2");
-  title.innerText = newTitle.value;
-  let body = document.createElement("p");
-  body.innerText = newBody.value;
-  let tags = document.createElement("span");
-  tags.innerText = newTags.value;
-  let react = document.createElement("div");
-  let img = document.createElement("img");
-  img.src = "images/thumb.jpg";
-  let likes = document.createElement("button");
-  likes.innerText = 0;
-  react.append(img, likes);
-  react.addEventListener("click", () => {
-    likes.innerText = parseInt(likes.innerText) + 1;
-  });
-  newPost.append(title, body, tags, react);
-  mainPage.append(newPost);
-  newTitle.value = "";
-  newBody.value = "";
-  newTags.value = "";
+  else {
+    newMsg.title = newTitle.value;
+    newMsg.body = newBody.value; 
+    newMsg.tags = newTags.value;
+    newMsg.reactions = 0;
+    postStorage.push(newMsg);
+    renderPosts(postStorage);
+    for (i=0; i<postStorage.length; i++) {
+      localStorage.setItem("post" + i, JSON.stringify(postStorage[i]));
+    }
+  }
 }
+
+//localStorage.setItem("posts", JSON.stringify(postStorage));
+
+
