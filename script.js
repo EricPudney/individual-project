f9863bfcf1757e3c0b9ebac0e471fdbf72f511c6
pudders <ericpudney@gmail.com>
2023-11-22 11:42:08 +0100

    tags now added by checkbox, not input field

--- script.js
+++ script.js
@@ -8,21 +8,23 @@ let newMsg = {};
 // defines fields in form to add new posts
 const newTitle = document.getElementById("newTitle");
 const newBody = document.getElementById("newBody");
-const newTags = document.getElementById("newTags");
+//const newTags = document.getElementById("newTags");
+const newTags = document.getElementsByClassName("tagbox");
 const addPost = document.getElementById("addPost");
 addPost.addEventListener("click", addNew);
 
-// gets initial data from either local storage OR dummyJSON and adds it to array
+// gets initial data from local storage and adds it to array
 if (localStorage.length > 0) {
   for (let i = 0; i < localStorage.length; i++) {
     let item = localStorage.getItem(localStorage.key(i));
     let obj = JSON.parse(item);
     postStorage.push(obj);
   }
-  // sorts stored posts by likes and renders them
+// sorts stored posts by likes and renders them
   postStorage.sort((a, b) => b.reactions - a.reactions);
   renderPosts(postStorage);
 }
+// if no data is found in local storage, it is fetched from dummyJSON here
 else {
   fetch('https://dummyjson.com/posts')
     .then(res => res.json())
@@ -31,14 +33,19 @@ else {
     });
 }
 
-
-// populates array, sorts it by likes, and sends it to be rendered
+// populates array from JSON data, sorts it by likes, and sends it to be rendered
 function fillArray(data) {
   for (let obj of data) {
     const newObj = {};
     newObj.title = obj.title;
-    newObj.body = obj.body;
-    newObj.tags = obj.tags;
+    newObj.body = obj.body; 
+//the 5 lines below puts commas and spaces between tags - done below for new posts
+    let text = "";
+    for (i = 0; i < obj.tags.length; i++) {
+      text += obj.tags[i] + ", ";
+    }
+    text = text.slice(0, text.length - 2);
+    newObj.tags = text;
     newObj.reactions = obj.reactions;
     postStorage.push(newObj);
   }
@@ -56,13 +63,6 @@ function renderPosts(arr) {
     let body = document.createElement("p");
     body.innerText = arr[i].body;
     let tags = document.createElement("span");
-    //puts commas and spaces between tags - doesn't work with new posts
-    /*let text = "";
-    for (j = 0; j < arr[i].tags.length; j++) {
-      text = text + arr[i].tags[j] + ", ";
-    }
-    text = text.slice(0, text.length - 2);
-    tags.innerText = text;*/
     tags.innerText = arr[i].tags;
     //creates clickable like button and counter
     let react = document.createElement("div");
@@ -81,59 +81,51 @@ function renderPosts(arr) {
   }
 }
 
-// turns posts into HTML objects - previously used to create posts directly from JSON object
-/*function createPosts(data) {
-  for (let obj of data) {
-    let newPost = document.createElement("div");
-    newPost.classList.add("post");
-    let title = document.createElement("h2");
-    title.innerText = obj.title;
-    let body = document.createElement("p");
-    body.innerText = obj.body;
-    let tags = document.createElement("span");
-    //puts commas and spaces between tags
-    let text = "";
-    for (i = 0; i < obj.tags.length; i++) {
-      text = text + obj.tags[i] + ", ";
-    }
-    text = text.slice(0, text.length - 2);
-    tags.innerText = text;
-    //creates likes button and counter
-    let react = document.createElement("div");
-    let img = document.createElement("img");
-    img.src = "images/thumb.jpg";
-    let likes = document.createElement("button");
-    likes.innerText = obj.reactions;
-    react.append(img, likes);
-    react.addEventListener("click", () => {
-      likes.innerText = parseInt(likes.innerText) + 1;
-    });
-    newPost.append(title, body, tags, react);
-    mainPage.append(newPost);
-  }
-}*/
-
 // adds new posts and saves array of posts to local storage
 function addNew() {
-  if (newTitle.value === "" || newBody.value === "" || newTags.value === "") {
+// checks text fields have not been left blank
+  if (newTitle.value === "" || newBody.value === "") {
+    alert ("Please fill in the required fields");
     return;
   }
+/* defines new post object, newMsg, to be added to array from user input.
+ Tags dealt with first to avoid unnecessarily setting other values before 
+ input has been checked for tags. */
   else {
+    let newTagtxt = "";
+    let tagEntered = false;
+    for (let i = 0; i < newTags.length; i++) {
+      if (newTags[i].checked === true) {
+        if (!tagEntered) {
+          newTagtxt += newTags[i].name;
+          tagEntered = true;
+        }
+        else {
+          newTagtxt += (", " + newTags[i].name);
+        }
+    }
+    }
+/* checks for 1+ tags before adding new post at the start of the array/top of the page
+ (on refresh, new posts are sorted by reactions along with all other posts) - this seemed 
+ to me to be the best way to handle new posts */
+    if (!tagEntered) {
+      alert("Please choose at least one tag");
+      return;
+  }
+    newMsg.tags = newTagtxt;
     newMsg.title = newTitle.value;
     newMsg.body = newBody.value;
-    newMsg.tags = newTags.value;
     newMsg.reactions = 0;
     postStorage.unshift(newMsg);
     mainPage.innerHTML = "";
     newTitle.value = "";
     newBody.value = "";
-    newTags.value = "";
     renderPosts(postStorage);
     savePosts();
   }
 }
 
-// saves posts to local storage (after new post added or reaction)
+// saves posts to local storage (after reaction or new post added)
 function savePosts() {
   localStorage.clear();
   for (i = 0; i < postStorage.length; i++) {
